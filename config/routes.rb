80588387e056f50c69114c033f88c1e2e7a1b18a
hiroki tamura <hiirookii0625@gmail.com>
2020-08-31 14:43:30 +0900

    favorites一覧画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :favorites, only: [:create, :destroy]
+  get '/favorites/index', to: 'favorites#index'
   resources :pictures do
     collection do
       post :confirm
