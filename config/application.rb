2bdb263ee7859b01a27a8747ef89021bcb9ab2c3
hiroki tamura <hiirookii0625@gmail.com>
2020-09-01 07:54:31 +0900

    タイムゾーンの設定を日本に変更

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Instaapp
       g.assets false
       g.helper false
     end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
