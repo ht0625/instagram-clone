26755c11e86a853d63ddee471ba80e72e8a72a8f
hiroki tamura <hiirookii0625@gmail.com>
2020-09-01 12:35:41 +0900

    userバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   #validates :password, presence: true, length: { minimum: 6 }
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 } , on: :create
 end
